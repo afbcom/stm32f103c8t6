pieis2pi/Teacup_Firmware

STM32F411: get FastIO for writing into place.

pinio.h
@@ -54,7 +54,7 @@
   /// Disable pullup resistor.
   #define _PULLUP_OFF(IO)  _WRITE(IO, 0)
 
-#elif defined __ARMEL__
+#elif defined __ARM_LPC1114__
 
   /**
     The LPC1114 supports bit-banding by mapping the bit mask to the address.
@@ -98,6 +98,32 @@
   #define _PULLUP_OFF(IO) \
     do { \
       LPC_IOCON->IO ## _CMSIS = (IO ## _OUTPUT | IO_MODEMASK_INACTIVE); \
+    } while (0)
+
+#elif defined __ARM_STM32F411__
+  /**
+  */
+  /// Write to a pin.
+  #define _WRITE(IO, v) \
+    do { \
+      if (v) \
+      	IO ## _PORT->BSRRL = MASK(IO ## _PIN); \
+      else \
+      	IO ## _PORT->BSRRH = MASK(IO ## _PIN); \
+    } while (0)
+
+  /** Set pin as output.
+      - reset Pullup
+      - reset direction mode
+      - set output
+      - set speed
+  */
+  #define _SET_OUTPUT(IO) \
+    do { \
+      IO ## _PORT->PUPDR &= ~(3 << ((IO ## _PIN) << 1)); \
+      IO ## _PORT->MODER &= ~(3 << ((IO ## _PIN) << 1)); \
+      IO ## _PORT->MODER |= (1 << ((IO ## _PIN) << 1)); \
+      IO ## _PORT->OSPEEDR |= (3 << ((IO ## _PIN) << 1)); \
     } while (0)
 
 #elif defined SIMULATOR

afbcom/stm32f103c8t6

pinio.h
@@ -63,7 +63,7 @@
   /// Disable pullup resistor.
   #define _PULL_OFF(IO)    _WRITE(IO, 0)
 
-#elif defined __ARMEL__
+#elif defined __ARM_LPC1114__
 
   /**
     The LPC1114 supports bit-banding by mapping the bit mask to the address.
@@ -112,6 +112,32 @@
   #define _PULL_OFF(IO) \
     do { \
       LPC_IOCON->IO ## _CMSIS = (IO ## _OUTPUT | IO_MODEMASK_INACTIVE); \
+    } while (0)
+
+#elif defined __ARM_STM32F411__
+  /**
+  */
+  /// Write to a pin.
+  #define _WRITE(IO, v) \
+    do { \
+      if (v) \
+      	IO ## _PORT->BSRR = MASK(IO ## _PIN); \
+      else \
+      	IO ## _PORT->BSRR = MASK((IO ## _PIN + 16)); \
+    } while (0)
+
+  /** Set pin as output.
+      - reset Pullup
+      - reset direction mode
+      - set output
+      - set speed
+  */
+  #define _SET_OUTPUT(IO) \
+    do { \
+      IO ## _PORT->PUPDR &= ~(3 << ((IO ## _PIN) << 1)); \
+      IO ## _PORT->MODER &= ~(3 << ((IO ## _PIN) << 1)); \
+      IO ## _PORT->MODER |= (1 << ((IO ## _PIN) << 1)); \
+      IO ## _PORT->OSPEEDR |= (3 << ((IO ## _PIN) << 1)); \
     } while (0)
 
 #elif defined SIMULATOR